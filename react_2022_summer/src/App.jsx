439376247d979c6e258eb77581a94ed2590396fd
Orca1110 <hoony4509@naver.com>
2022-06-27 18:37:02 +0900

    chapter2_2

--- react_2022_summer/src/App.jsx
+++ react_2022_summer/src/App.jsx
@@ -39,7 +39,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-    return <></>;
+    return <>
+    
+    </>;
 }
 
 export default App;
