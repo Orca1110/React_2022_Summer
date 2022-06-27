4cb0fd06e1dfa9486732ba4392e0869d161289e0
Orca1110 <hoony4509@naver.com>
2022-06-27 18:27:57 +0900

    after chapter 2

--- react_2022_summer/src/Hello.jsx
+++ react_2022_summer/src/Hello.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import './Hello.css';
+import styled from 'styled-components';
 
 function Hello(){
-    return <div> Hello World! </div>;
+    const PracticeStyle ={
+        marginTop : '10px',
+        backgroundColor : 'blue',
+    };
+
+    const StyledButton = styled.button`
+        color : red;
+        background-color : gray;
+    `
+
+    return (
+    <> 
+        <div style={PracticeStyle}> Hello World! </div>
+        <div className="red"> Hello World!</div>
+    </>
+    // <StyledButton>My Button</StyledButton>
+    );
 }
 export default Hello;
