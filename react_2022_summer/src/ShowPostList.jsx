a317e257791eda66ccb98d034c6ef8d79542dbfa
Orca1110 <hoony4509@naver.com>
2022-07-07 16:02:21 +0900

    Chapter6 List Page

--- react_2022_summer/src/ShowPostList.jsx
+++ react_2022_summer/src/ShowPostList.jsx
@@ -22,41 +22,44 @@ import axios from "axios";
 import EachPost from "./EachPost";
 import { useNavigate } from "react-router-dom";
 
-// const initialPostList = [
-//     { id: 1, title: "학보, 시사N 대학기자상 취재" },
-//     { id: 2, title: "학보, 시사N 대학기자상 취재" },
-//     { id: 3, title: "학보, 시사N 대학기자상 취재" },
-// ];
-
 function ShowPostList({ apiUrl }) {
     const [loading, setLoading] = useState(true);
-    const [isPost, setIsPost] = useState(false);
     const [postList, setPostList] = useState([]);
-
-    const addPost = useCallback(() => {
-        setPostList((postList) => [
-            ...postList,
-            { id: 4, title: "학보, 시사N 대학기자상 취재", replCount: 21 },
-        ]);
-    }, [postList]);
+    const [page, setPage] = useState(1);
+    const [pages, setPages] = useState([]);
 
     const navigate = useNavigate();
     const goWrite = () => {
         navigate("/write");
     };
 
-    useEffect(() => {
-        axios.get(`${apiUrl}list/?page=1&page_size=10`).then((response) => {
-            console.log(response.data);
-            setPostList(response.data.results);
-            setLoading(false);
-        });
-    }, []);
+    const getPostList = useCallback(() => {
+        setLoading(true);
+        axios
+            .get(`${apiUrl}list/?page=${page}&page_size=10`)
+            .then((response) => {
+                const lastPage = Math.ceil(response.data.count / 10);
+                const tempPages = [];
+                for (let i = 1; i <= lastPage; i++) {
+                    tempPages.push(i);
+                }
+                setPages(tempPages);
+
+                setPostList(response.data.results);
+                setLoading(false);
+            });
+    });
+
+    useEffect(getPostList, [page]);
+
     return (
         <>
             <PostSection>
                 <PostTitleDiv>
-                    <FontAwesomeIcon onClick={addPost} icon={faArrowsRotate} />
+                    <FontAwesomeIcon
+                        onClick={getPostList}
+                        icon={faArrowsRotate}
+                    />
                     <PostTitle>익명게시판</PostTitle>
                     <CursorDiv>
                         <FontAwesomeIcon
@@ -71,7 +74,7 @@ function ShowPostList({ apiUrl }) {
                             <LoadingImg src={"./loading.svg"} />
                             {/* loading.io */}
                         </LoadingDiv>
-                    ) : isPost ? (
+                    ) : postList.length === 0 ? (
                         <LoadingDiv>아직 기록된 글이 없습니다.</LoadingDiv>
                     ) : (
                         <ul>
@@ -88,12 +91,30 @@ function ShowPostList({ apiUrl }) {
             </PostSection>
 
             <PagingSection>
-                <PagenumberDiv>
+                <PagenumberDiv
+                    onClick={() => {
+                        if (page > 1) {
+                            setPage(page - 1);
+                        }
+                    }}
+                >
                     <FontAwesomeIcon icon={faArrowLeft} />
                 </PagenumberDiv>
-                <PagenumberDiv>2</PagenumberDiv>
-
-                <PagenumberDiv>
+                {pages.map((pageNum) => (
+                    <PagenumberDiv
+                        key={pageNum}
+                        onClick={() => setPage(pageNum)}
+                    >
+                        {pageNum}
+                    </PagenumberDiv>
+                ))}
+                <PagenumberDiv
+                    onClick={() => {
+                        if (pages.length > page) {
+                            setPage(page + 1);
+                        }
+                    }}
+                >
                     <FontAwesomeIcon icon={faArrowRight} />
                 </PagenumberDiv>
             </PagingSection>
